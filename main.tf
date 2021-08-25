9b2ed5bfa1f01e4fa5c5d6662919df34c2993c6b
Matthew Bentley <mbentley@hey.com>
2021-08-25 00:03:20 -0700

    fix perms

--- main.tf
+++ main.tf
@@ -2,14 +2,14 @@ provider "aws" {
   region = var.region
 }
 
-#resource "aws_route53_zone" "mtbwtf" {
-#  name = "mtb.wtf"
-#}
-#
-#resource "aws_route53_record" "test" {
-#  zone_id = aws_route53_zone.mtbwtf.zone_id
-#  name = "mtf.wtf"
-#  type = "A"
-#  ttl = "300"
-#  records = ["0.0.0.0"]
-#}
+resource "aws_route53_zone" "mtbwtf" {
+  name = "mtb.wtf"
+}
+
+resource "aws_route53_record" "test" {
+  zone_id = aws_route53_zone.mtbwtf.zone_id
+  name = "mtf.wtf"
+  type = "A"
+  ttl = "300"
+  records = ["0.0.0.0"]
+}
